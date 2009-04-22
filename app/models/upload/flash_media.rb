8737d827ca304be2cc3d36d5013cc30899c55518
jsanchez <jsanchez@4aa487b5-cee0-4876-81ae-a3a41dd53ca9>
2009-04-22 20:42:43 +0000

    Adding flash handling
    git-svn-id: http://svn.growthaccelerationpartners.net/dpadmanager/trunk@9 4aa487b5-cee0-4876-81ae-a3a41dd53ca9

--- app/models/upload/flash_media.rb
+++ app/models/upload/flash_media.rb
@@ -1,3 +1,31 @@
 class FlashMedia < Media
+  property :height, Integer
+  property :width,  Integer
+
+  def data=(data)
+    extract_dimensions(data[:tempfile])
+
+    super(data)
+  end
+
+  def extract_dimensions(tempfile)
+    temp_path = Merb.dir_for(:uploads) + "/flash_image.png"
+    ImageScience.with_image temp_path do |img|
+      self.height = img.height
+      self.width = img.width
+    end
+  end
   
+    
+    def make_thumb
+      unless File.exists?(thumb_dir)
+        FileUtils.mkdir_p(thumb_dir, :mode => 0o755)
+      end
+      temp_path = Merb.dir_for(:uploads) + "/flash_image.png"
+      ImageScience.with_image temp_path do |img|
+        img.thumbnail(150) do |thumb|
+          thumb.save thumb_path
+        end
+      end
+    end
 end
