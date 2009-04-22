9a981eed114b473ab9b6bc17f5ae780ef5c266ba
jsanchez <jsanchez@4aa487b5-cee0-4876-81ae-a3a41dd53ca9>
2009-04-22 20:33:03 +0000

    Adding flash handling
    git-svn-id: http://svn.growthaccelerationpartners.net/dpadmanager/trunk@7 4aa487b5-cee0-4876-81ae-a3a41dd53ca9

--- app/models/upload/flash_thumb.rb
+++ app/models/upload/flash_thumb.rb
@@ -1,16 +1,21 @@
-require 'ruby-debug'
-
 class FlashThumb < Thumb
   property :height, Integer
   property :width,  Integer
 
+  def path
+    @path ||= unless digest.nil? || extname.nil?
+      UploadManager.bucketize(Merb.dir_for(:uploads) - Merb.dir_for(:public), filenameThumb)
+    end
+  end
+  
+  def full_path
+    @full_path ||= unless digest.nil? || extname.nil?
+      UploadManager.bucketize(Merb.dir_for(:uploads), filenameThumb)
+    end
+  end
 
   def write_to_bucket
-    debugger
-    puts("image_thumb :S")
     temp_path = Merb.dir_for(:uploads) + "/flash_image.png"
-    puts(temp_path)
-    
     unless @data.nil?
       ImageScience.with_image temp_path do |img|
         img.thumbnail(150) do |thumb|
